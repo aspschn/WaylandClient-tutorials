cdfed3d6f8e89016dcac0bc3796399f763125e5f
Yujŏnja <hardboiled65@gmail.com>
2021-12-08 11:29:15 +0900

    Working with subsurface.

--- seat2/main.c
+++ seat2/main.c
@@ -28,6 +28,10 @@ struct wl_buffer *buffer;
 
 struct wl_surface *surface2;
 struct wl_callback *frame_callback2;
+struct wl_region *region2;
+
+struct wl_subcompositor *subcompositor;
+struct wl_subsurface *subsurface;
 
 void *shm_data;
 
@@ -124,6 +128,9 @@ static void pointer_motion_handler(void *data, struct wl_pointer *pointer,
         uint32_t time, wl_fixed_t sx, wl_fixed_t sy)
 {
 //    fprintf(stderr, "Pointer moved at %d %d\n", sx, sy);
+//    if (sx > 49152 && sy < 2816) {
+//        zxdg_surface_v6_destroy(xdg_surface);
+//    }
 }
 
 static void pointer_button_handler(void *data, struct wl_pointer *wl_pointer,
@@ -213,7 +220,6 @@ static void redraw(void *data, struct wl_callback *callback, uint32_t time)
 
 static void redraw2(void *data, struct wl_callback *callback, uint32_t time)
 {
-    printf("Redrawing 2\n");
     wl_callback_destroy(frame_callback2);
     wl_surface_damage(surface2, 0, 0, 480, 360);
     paint_pixels2();
@@ -237,115 +243,22 @@ static struct wl_callback_listener configure_callback_listener = {
     configure_callback,
 };
 
-static struct wl_buffer* create_buffer();
+static struct wl_buffer* create_buffer(int width, int height);
 
 static void create_window()
 {
-    /*
-    egl_window = wl_egl_window_create(surface, 480, 360);
-    if (egl_window == EGL_NO_SURFACE) {
-        fprintf(stderr, "Can't create egl window\n");
-        exit(1);
-    }
-
-    egl_surface = eglCreateWindowSurface(egl_display, egl_conf, egl_window,
-        NULL);
-
-    if (eglMakeCurrent(egl_display, egl_surface, egl_surface, egl_context)) {
-        fprintf(stderr, "Made current\n");
-    }
-
-    glClearColor(1.0, 1.0, 0.0, 0.1);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
-
-    if (eglSwapBuffers(egl_display, egl_surface)) {
-        fprintf(stderr, "Swapped buffers\n");
-    } else {
-        fprintf(stderr, "Swapped buffers failed\n");
-        exit(1);
-    }
-
-    wl_display_dispatch(display);
-    wl_display_roundtrip(display);
-    */
-    buffer = create_buffer();
+    buffer = create_buffer(480, 360);
 
     wl_surface_attach(surface, buffer, 0, 0);
     wl_surface_commit(surface);
 }
 
-static void change_color()
+static void create_surface2()
 {
-    fprintf(stderr, "on change_color()\n");
+    buffer = create_buffer(200, 200);
 
-    glClearColor(1.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
-
-    eglSwapBuffers(egl_display, egl_surface);
-}
-
-static void init_egl()
-{
-    EGLint major, minor, count, n, size;
-    EGLConfig *configs;
-    EGLBoolean ret;
-    EGLint config_attribs[] = {
-        EGL_SURFACE_TYPE,
-        EGL_WINDOW_BIT,
-        EGL_RED_SIZE,
-        8,
-        EGL_GREEN_SIZE,
-        8,
-        EGL_BLUE_SIZE,
-        8,
-        EGL_RENDERABLE_TYPE,
-        EGL_OPENGL_ES2_BIT,
-        EGL_NONE,
-    };
-
-    static const EGLint context_attribs[] = {
-        EGL_CONTEXT_CLIENT_VERSION,
-        2,
-        EGL_NONE,
-    };
-
-    egl_display = eglGetDisplay((EGLNativeDisplayType)display);
-    if (egl_display == EGL_NO_DISPLAY) {
-        exit(1);
-    }
-
-    if (eglInitialize(egl_display, &major, &minor) != EGL_TRUE) {
-        exit(1);
-    }
-    printf("EGL major: %d, minor: %d\n", major, minor);
-
-    if (!eglBindAPI(EGL_OPENGL_ES_API)) {
-        fprintf(stderr, "Can't bind API\n");
-        exit(1);
-    }
-
-    eglGetConfigs(egl_display, NULL, 0, &count);
-    printf("EGL has %d configs\n", count);
-
-    configs = calloc(count, sizeof *configs);
-
-    ret = eglChooseConfig(egl_display, config_attribs, configs, count, &n);
-    fprintf(stderr, "eglChooseConfig ret: %d\n", ret);
-
-    for (int i = 0; i < n; ++i) {
-        eglGetConfigAttrib(egl_display, configs[i], EGL_BUFFER_SIZE, &size);
-        printf("Buffer size for config %d is %d\n", i, size);
-        eglGetConfigAttrib(egl_display, configs[i], EGL_RED_SIZE, &size);
-        printf("Red size for config %d is %d\n", i, size);
-
-        egl_conf = configs[i];
-        break;
-    }
-
-    egl_context = eglCreateContext(egl_display, egl_conf, EGL_NO_CONTEXT,
-        context_attribs);
+    wl_surface_attach(surface2, buffer, 0, 0);
+    wl_surface_commit(surface2);
 }
 
 //==============
@@ -423,11 +336,11 @@ static void paint_pixels2()
     }
 }
 
-static struct wl_buffer* create_buffer()
+static struct wl_buffer* create_buffer(int width, int height)
 {
     struct wl_shm_pool *pool;
-    int stride = 480 * 4;
-    int size = stride * 360;
+    int stride = width * 4;
+    int size = stride * height;
     int fd;
     struct wl_buffer *buff;
 
@@ -445,7 +358,7 @@ static struct wl_buffer* create_buffer()
     }
 
     pool = wl_shm_create_pool(shm, fd, size);
-    buff = wl_shm_pool_create_buffer(pool, 0, 480, 360, stride,
+    buff = wl_shm_pool_create_buffer(pool, 0, width, height, stride,
         WL_SHM_FORMAT_XRGB8888);
 
     wl_shm_pool_destroy(pool);
@@ -458,6 +371,9 @@ static struct wl_buffer* create_buffer()
 static void global_registry_handler(void *data, struct wl_registry *registry,
         uint32_t id, const char *interface, uint32_t version)
 {
+    (void)data;
+    (void)version;
+
     if (strcmp(interface, "wl_seat") == 0) {
         seat = wl_registry_bind(registry, id, &wl_seat_interface, 1);
         wl_seat_add_listener(seat, &seat_listener, NULL);
@@ -477,6 +393,11 @@ static void global_registry_handler(void *data, struct wl_registry *registry,
             fprintf(stderr, "Can't get default cursor.\n");
             exit(1);
         }
+    } else if (strcmp(interface, "wl_subcompositor") == 0) {
+        subcompositor = wl_registry_bind(registry, id,
+            &wl_subcompositor_interface, 1);
+    } else {
+        fprintf(stderr, "Interface <%s>\n", interface);
     }
 }
 
@@ -514,6 +435,10 @@ int main(int argc, char *argv[])
 
     if (compositor == NULL || xdg_shell == NULL) {
         fprintf(stderr, "Can't find compositor or xdg_shell.\n");
+        exit(1);
+    }
+    if (subcompositor == NULL) {
+        fprintf(stderr, "Can't find subcompositor.\n");
         exit(1);
     }
 
@@ -556,6 +481,11 @@ int main(int argc, char *argv[])
     } else {
         fprintf(stderr, "surface2 created: %p\n", surface2);
     }
+    wl_surface_commit(surface2);
+
+    // Subsurface
+    subsurface = wl_subcompositor_get_subsurface(subcompositor,
+        surface2, surface);
 
     callback = wl_display_sync(display);
     wl_callback_add_listener(callback, &configure_callback_listener, NULL);
@@ -565,19 +495,23 @@ int main(int argc, char *argv[])
     frame_callback2 = wl_surface_frame(surface2);
     wl_callback_add_listener(frame_callback2, &frame_listener2, NULL);
 
-    init_egl();
+
+    region2 = wl_compositor_create_region(compositor);
+    wl_region_add(region2, 0, 0, 100, 50);
+    wl_surface_set_input_region(surface2, region2);
+
+
     create_window();
+//    create_surface2();
     paint_pixels();
 //    redraw(NULL, NULL, 0);
-    redraw2(NULL, NULL, 0);
+//    redraw2(NULL, NULL, 0);
 
     cursor_surface = wl_compositor_create_surface(compositor);
 
 
 
     wl_surface_commit(surface);
-
-//    change_color();
 
     while (wl_display_dispatch(display) != -1) {
         ;
