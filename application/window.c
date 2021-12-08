425d8ca6ce98e23457711260783975833dde43e7
Yujŏnja <hardboiled65@gmail.com>
2021-12-08 17:53:55 +0900

    Add drawing functions.

--- application/window.c
+++ application/window.c
@@ -2,7 +2,11 @@
 
 #include <stdio.h>
 
+#include <sys/mman.h>
+#include <unistd.h>
+
 #include "application.h"
+#include "utils.h"
 
 //==============
 // Xdg
@@ -48,6 +52,57 @@ static const struct zxdg_toplevel_v6_listener xdg_toplevel_listener = {
     .configure = xdg_toplevel_configure_handler,
     .close = xdg_toplevel_close_handler,
 };
+
+//=============
+// Drawing
+//=============
+static struct wl_buffer* create_buffer(int width, int height,
+        void *shm_data, struct wl_shm *shm)
+{
+    struct wl_shm_pool *pool;
+    int stride = width * 4;
+    int size = stride * height;
+    int fd;
+    struct wl_buffer *buff;
+
+    fd = os_create_anonymous_file(size);
+    if (fd < 0) {
+        fprintf(stderr, "Creating a buffer file for %d B failed: %m\n",
+            size);
+        exit(1);
+    }
+
+    shm_data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (shm_data == MAP_FAILED) {
+        close(fd);
+        exit(1);
+    }
+
+    pool = wl_shm_create_pool(shm, fd, size);
+    buff = wl_shm_pool_create_buffer(pool, 0, width, height, stride,
+        WL_SHM_FORMAT_XRGB8888);
+
+    wl_shm_pool_destroy(pool);
+    return buff;
+}
+
+static void paint_pixels(int size, void *shm_data)
+{
+    uint32_t *pixel = shm_data;
+
+    for (int n = 0; n < size; ++n) {
+        *pixel++ = 0xff0000;
+    }
+}
+
+static void create_window_surface(bl_window *window,
+        struct wl_buffer *buffer, void *shm_data, struct wl_shm *shm)
+{
+    buffer = create_buffer(window->width, window->height, shm_data, shm);
+
+    wl_surface_attach(window->surface, buffer, 0, 0);
+    wl_surface_commit(window->surface);
+}
 
 //=============
 // Window
