0120e78056278e13c10344d1afc864bfe395d760
Yujŏnja <hardboiled65@gmail.com>
2022-03-18 12:06:35 +0900

    Change connect tutorial for example code.

--- connect/main.c
+++ connect/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <wayland-client.h>
 
 struct wl_display *display = NULL;
+struct wl_registry *registry = NULL;
 struct wl_compositor *compositor = NULL;
 
 //==============
@@ -12,29 +14,23 @@ struct wl_compositor *compositor = NULL;
 static void global_registry_handler(void *data, struct wl_registry *registry,
         uint32_t id, const char *interface, uint32_t version)
 {
+    printf("Got a registry event for <%s>, id: %d, version: %d.\n",
+        interface, id, version);
     if (strcmp(interface, "wl_compositor") == 0) {
-        fprintf(stderr, "Interface is <wl_compositor>.\n");
-        compositor = wl_registry_bind(
-            registry,
-            id,
-            &wl_compositor_interface,
-            version
-        );
-    } else {
-        printf("(%d) Got a registry event for <%s> id <%d>\n",
-            version, interface, id);
+        compositor = wl_registry_bind(registry,
+            id, &wl_compositor_interface, 4);
     }
 }
 
-static void global_registry_remover(void *data, struct wl_registry *registry,
-        uint32_t id)
+static void global_registry_remove_handler(void *data,
+        struct wl_registry *registry, uint32_t id)
 {
     printf("Got a registry losing event for <%d>\n", id);
 }
 
 static const struct wl_registry_listener registry_listener = {
-    global_registry_handler,
-    global_registry_remover
+    .global = global_registry_handler,
+    .global_remove = global_registry_remove_handler,
 };
 
 int main(int argc, char *argv[])
@@ -44,12 +40,12 @@ int main(int argc, char *argv[])
 
     display = wl_display_connect(NULL);
     if (display == NULL) {
-        fprintf(stderr, "Can't connect to display.\n");
+        fprintf(stderr, "Failed to connect to display.\n");
         exit(1);
     }
     printf("Connected to display.\n");
 
-    struct wl_registry *registry = wl_display_get_registry(display);
+    registry = wl_display_get_registry(display);
     wl_registry_add_listener(registry, &registry_listener, NULL);
 
     wl_display_dispatch(display);
@@ -61,7 +57,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Can't find compositor.\n");
         exit(1);
     } else {
-        fprintf(stderr, "Found compositor!\n");
+        printf("Found compositor!\n");
     }
 
     wl_display_disconnect(display);
