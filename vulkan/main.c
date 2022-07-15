0cea683540c9dd3147f65be4505b75f898b72bd7
Yujŏnja <hardboiled65@gmail.com>
2022-07-15 10:31:04 +0900

    Write many pipeline creation infos.

--- vulkan/main.c
+++ vulkan/main.c
@@ -88,10 +88,10 @@ VkPipelineShaderStageCreateInfo vulkan_frag_shader_stage_create_info;
 VkPipelineShaderStageCreateInfo vulkan_shader_stage_create_info;
 VkPipelineVertexInputStateCreateInfo vulkan_vert_input_state_create_info;
 VkPipelineInputAssemblyStateCreateInfo vulkan_input_assembly_state_create_info;
-VkPipelineViewportStateCreateInfo vulkan_vewport_state_create_info;
+VkPipelineViewportStateCreateInfo vulkan_viewport_state_create_info;
 VkPipelineRasterizationStateCreateInfo vulkan_rasterization_state_create_info;
 VkPipelineMultisampleStateCreateInfo vulkan_multisample_state_create_info;
-VkPipelineColorBlendAttachmentState vulkan_clor_blend_attachment_state;
+VkPipelineColorBlendAttachmentState vulkan_color_blend_attachment_state;
 VkPipelineColorBlendStateCreateInfo vulkan_color_blend_state_create_info;
 VkDynamicState dynamic_states[] = {
     VK_DYNAMIC_STATE_VIEWPORT,
@@ -99,6 +99,7 @@ VkDynamicState dynamic_states[] = {
 };
 VkPipelineDynamicStateCreateInfo vulkan_dynamic_state_create_info;
 VkPipelineLayoutCreateInfo vulkan_layout_create_info;
+VkPipelineLayout vulkan_layout = NULL;
 
 
 EGLDisplay egl_display;
@@ -712,11 +713,88 @@ static void create_vulkan_graphics_pipeline()
     vulkan_frag_shader_stage_create_info.module = frag_shader_module;
     vulkan_frag_shader_stage_create_info.pName = "main";
 
+    // Shader stage.
+    vulkan_shader_stage_create_info;
+
+    // Vertex input.
+    vulkan_vert_input_state_create_info.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vulkan_vert_input_state_create_info.vertexBindingDescriptionCount = 0;
+    vulkan_vert_input_state_create_info.vertexAttributeDescriptionCount = 0;
+
+    // Input assembly.
+    vulkan_input_assembly_state_create_info.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    vulkan_input_assembly_state_create_info.topology =
+        VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    vulkan_input_assembly_state_create_info.primitiveRestartEnable = VK_FALSE;
+
+    // Viewport.
+    vulkan_viewport_state_create_info.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    vulkan_viewport_state_create_info.viewportCount = 1;
+    vulkan_viewport_state_create_info.scissorCount = 1;
+
+    // Rasterization.
+    vulkan_rasterization_state_create_info.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    vulkan_rasterization_state_create_info.depthClampEnable = VK_FALSE;
+    vulkan_rasterization_state_create_info.rasterizerDiscardEnable = VK_FALSE;
+    vulkan_rasterization_state_create_info.polygonMode = VK_POLYGON_MODE_FILL;
+    vulkan_rasterization_state_create_info.lineWidth = 1.0f;
+    vulkan_rasterization_state_create_info.cullMode = VK_CULL_MODE_BACK_BIT;
+    vulkan_rasterization_state_create_info.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    vulkan_rasterization_state_create_info.depthBiasEnable = VK_FALSE;
+
+    // Multisampling.
+    vulkan_multisample_state_create_info.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    vulkan_multisample_state_create_info.sampleShadingEnable = VK_FALSE;
+    vulkan_multisample_state_create_info.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+    // Color blend attachment.
+    vulkan_color_blend_attachment_state.colorWriteMask =
+        VK_COLOR_COMPONENT_R_BIT |
+        VK_COLOR_COMPONENT_G_BIT |
+        VK_COLOR_COMPONENT_B_BIT |
+        VK_COLOR_COMPONENT_A_BIT;
+    vulkan_color_blend_attachment_state.blendEnable = VK_FALSE;
+
+    // Color blending.
+    vulkan_color_blend_state_create_info.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    vulkan_color_blend_state_create_info.logicOpEnable = VK_FALSE;
+    vulkan_color_blend_state_create_info.logicOp = VK_LOGIC_OP_COPY;
+    vulkan_color_blend_state_create_info.attachmentCount = 1;
+    vulkan_color_blend_state_create_info.pAttachments =
+        &vulkan_color_blend_attachment_state;
+    vulkan_color_blend_state_create_info.blendConstants[0] = 0.0f;
+    vulkan_color_blend_state_create_info.blendConstants[1] = 0.0f;
+    vulkan_color_blend_state_create_info.blendConstants[2] = 0.0f;
+    vulkan_color_blend_state_create_info.blendConstants[3] = 0.0f;
+
     // Dynamic states.
     VkPipelineDynamicStateCreateInfo create_info;
     create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
     create_info.dynamicStateCount = 2;
     create_info.pDynamicStates = dynamic_states;
+
+    // Layout.
+    vulkan_layout_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    vulkan_layout_create_info.setLayoutCount = 0;
+    vulkan_layout_create_info.pushConstantRangeCount = 0;
+
+    result = vkCreatePipelineLayout(vulkan_device,
+        &vulkan_layout_create_info, NULL, &vulkan_layout);
+    if (result != VK_SUCCESS) {
+        fprintf(stderr, "Failed to create pipeline layout!\n");
+    }
+    fprintf(stderr, "Pipeline layout created.\n");
+
+    /*
+    vkDestroyShaderModule(vulkan_device, frag_shader_module, NULL);
+    vkDestroyShaderModule(vulkan_device, vert_shader_module, NULL);
+    */
 }
 
 //===========
