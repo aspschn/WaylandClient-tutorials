2a38ee227cc5b35144f5eee88b3a5552427815de
Yujŏnja <hardboiled65@gmail.com>
2022-07-16 16:42:53 +0900

    Remove unused code and testing.

--- vulkan/main.c
+++ vulkan/main.c
@@ -1077,71 +1077,6 @@ const struct xdg_toplevel_listener xdg_toplevel_listener = {
 // Output
 //==============
 
-static void output_geometry_handler(void *data,
-        struct wl_output *output, int32_t x, int32_t y,
-        int32_t physical_width, int32_t physical_height, int subpixel,
-        const char *make, const char *model, int transform)
-{
-    (void)data;
-
-    fprintf(stderr, "output_geometry_handler() - output: %p\n", output);
-    fprintf(stderr, " |- x: %d, y: %d\n", x, y);
-    fprintf(stderr, " |- phy width: %d, phy height: %d\n", physical_width, physical_height);
-    fprintf(stderr, " |- subpixel: %d\n", subpixel);
-    fprintf(stderr, " |- make: %s\n", make);
-    fprintf(stderr, " |- model: %s\n", model);
-    fprintf(stderr, " +- transform: %d\n", transform);
-}
-
-static void output_mode_handler(void *data,
-        struct wl_output *output, uint32_t flags,
-        int32_t width, int32_t height, int32_t refresh)
-{
-    (void)data;
-    (void)output;
-}
-
-static void output_done_handler(void *data,
-        struct wl_output *output)
-{
-    (void)data;
-    (void)output;
-}
-
-static void output_scale_handler(void *data,
-        struct wl_output *output, int32_t scale)
-{
-    (void)data;
-
-    fprintf(stderr, "output_scale_handler() - output: %p, scale: %d\n", output, scale);
-}
-
-static void output_name_handler(void *data,
-        struct wl_output *output, const char *name)
-{
-    (void)data;
-    (void)output;
-    fprintf(stderr, "output_name_handler() - output: %p, name: %s\n", output, name);
-}
-
-static void output_description_handler(void *data,
-        struct wl_output *output, const char *description)
-{
-    (void)data;
-    (void)output;
-    fprintf(stderr, "output_description_handler() - description: %s\n",
-        description);
-}
-
-static const struct wl_output_listener output_listener = {
-    .geometry = output_geometry_handler,
-    .mode = output_mode_handler,
-    .done = output_done_handler,
-    .scale = output_scale_handler,
-    .name = output_name_handler,
-    .description = output_description_handler,
-};
-
 //==============
 // Global
 //==============
@@ -1149,7 +1084,6 @@ static void global_registry_handler(void *data, struct wl_registry *registry,
         uint32_t id, const char *interface, uint32_t version)
 {
     if (strcmp(interface, "wl_compositor") == 0) {
-        fprintf(stderr, "Interface is <wl_compositor>.\n");
         compositor = wl_registry_bind(
             registry,
             id,
@@ -1159,14 +1093,7 @@ static void global_registry_handler(void *data, struct wl_registry *registry,
     } else if (strcmp(interface, "xdg_wm_base") == 0) {
         xdg_wm_base = wl_registry_bind(registry,
             id, &xdg_wm_base_interface, 1);
-    } else if (strcmp(interface, "wl_subcompositor") == 0) {
-        subcompositor = wl_registry_bind(registry,
-            id, &wl_subcompositor_interface, 1);
-    } else if (strcmp(interface, "wl_output") == 0) {
-        fprintf(stderr, "Interface is <wl_output> v%d\n", version);
-        output = wl_registry_bind(registry,
-            id, &wl_output_interface, 2);
-        wl_output_add_listener(output, &output_listener, NULL);
+        xdg_wm_base_add_listener(xdg_wm_base, &xdg_wm_base_listener, NULL);
     } else {
         printf("(%d) Got a registry event for <%s> id <%d>\n",
             version, interface, id);
@@ -1185,8 +1112,11 @@ static const struct wl_registry_listener registry_listener = {
 };
 
 
-static void get_server_references()
+int main(int argc, char *argv[])
 {
+    (void)argc;
+    (void)argv;
+
     display = wl_display_connect(NULL);
     if (display == NULL) {
         fprintf(stderr, "Can't connect to display.\n");
@@ -1197,51 +1127,26 @@ static void get_server_references()
     struct wl_registry *registry = wl_display_get_registry(display);
     wl_registry_add_listener(registry, &registry_listener, NULL);
 
-    wl_display_dispatch(display);
     wl_display_roundtrip(display);
+    fprintf(stderr, "wl_display_roundtrip()\n");
 
     if (compositor == NULL || xdg_wm_base == NULL) {
         fprintf(stderr, "Can't find compositor or xdg_wm_base.\n");
         exit(1);
-    } else {
-        fprintf(stderr, "Found compositor and xdg_wm_base.\n");
     }
-}
-
-int main(int argc, char *argv[])
-{
-    (void)argc;
-    (void)argv;
-
-    get_server_references();
 
     // Check surface.
-    fprintf(stderr, " - Checking surface...\n");
     surface = wl_compositor_create_surface(compositor);
     if (surface == NULL) {
         fprintf(stderr, "Can't create surface.\n");
         exit(1);
-    } else {
-        fprintf(stderr, "Created surface!\n");
-    }
-
-    xdg_wm_base_add_listener(xdg_wm_base, &xdg_wm_base_listener, NULL);
-
-    // Output.
-    fprintf(stderr, " - Checking output...\n");
-    if (output == NULL) {
-        fprintf(stderr, "Output is NULL!\n");
-        exit(1);
     }
 
     // Check xdg surface.
-    fprintf(stderr, " - Checking xdg surface...\n");
     xdg_surface = xdg_wm_base_get_xdg_surface(xdg_wm_base, surface);
     if (xdg_surface == NULL) {
         fprintf(stderr, "Can't create xdg surface.\n");
         exit(1);
-    } else {
-        fprintf(stderr, "Created xdg surface!\n");
     }
     xdg_surface_add_listener(xdg_surface, &xdg_surface_listener, NULL);
 
@@ -1257,15 +1162,6 @@ int main(int argc, char *argv[])
 
     wl_display_roundtrip(display);
 
-    /*
-    // create_opaque_region();
-    init_egl();
-    create_window();
-    if (init(&program_object) == 0) {
-        fprintf(stderr, "Error init!\n");
-    }
-    */
-
     // Vulkan init.
     init_vulkan();
     create_vulkan_window();
@@ -1279,11 +1175,12 @@ int main(int argc, char *argv[])
     create_vulkan_command_buffer();
     create_vulkan_sync_objects();
 
-    draw_frame();
+    // draw_frame();
 
     wl_surface_commit(surface);
 
     int res = wl_display_dispatch(display);
+    fprintf(stderr, "Initial dispatch.\n");
     while (res != -1) {
         res = wl_display_dispatch(display);
     }
